e50b4703aa13e63a0623c6466ba2a5119d829f38
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-07-29 16:16:20 -0700

    added more ascii

--- text-based-game.rb
+++ text-based-game.rb
@@ -13,7 +13,7 @@ end
 
 
 def cookie
-  cookie = "To breathe underwater you must eat a cookie. Do you want to eat the cookie? Yes or No "
+  cookie = "You see a random cookie on the ground. Do you want to eat the cookie? Yes or No "
   print_slow cookie
   gets.chomp
 end
@@ -29,7 +29,7 @@ end
 
 
 def eat_cookie
-  eat = "You walk in the ocean and see smack of jellyfish and a bale of sea turtles. Would you like to swim with the 'turtles' or the 'jellyfish'?"
+  eat = "You see smack of jellyfish and a bale of sea turtles. Would you like to swim with the 'turtles' or the 'jellyfish'?"
   print_slow eat
   gets.chomp
 end
@@ -47,46 +47,53 @@ end
 def mantaray_dolphin
   option = "You stop when you see a tunnel and at the end is some sunlight. You must choose to go 'right' or 'left'"
   print_slow option
-   gets.chomp
+  gets.chomp
 end
 
  def safe
   print_slow "You see the shore! You swim to the mainland and you are now safe. Congratulations! Time to get some lunch."
 end
 
-def render_beach
-  IO.foreach("Beach.txt") do |line|
+def render_ascii artFile
+  IO.foreach(artFile) do |line|
     puts line
   end
 end
 
-def dolphin
-  IO.foreach("dolphin.txt") do |line|
-    puts line
-  end
-end
 
-def manta_ray
-  IO.foreach("mantaray.txt") do |line|
-    puts line
-  end
-end
 
-def turtle
-  IO.foreach("turtle.txt") do |line|
-    puts line
-  end
-end
-
-render_beach
-
+render_ascii "Beach.txt"
 start_story_book
 answer = cookie
-yes_or_no(answer, "yes", "You can now breathe underwater", "You are gathering sea shells")
+
+if answer.downcase == "no"
+  render_ascii "mermaid.txt"
+end
+
+yes_or_no(answer, "yes", "You can now breathe underwater so you walk in the ocean.", "You gather sea shells and you make a necklace. You wear it and turn into a mermaid! You then go for a dive in the ocean.")
+
 answer = eat_cookie
-yes_or_no(answer, "turtles", "Hold on tight! We are going for a swim!", "Be aware! Don't get stung!")
+if answer == "turtles"
+  render_ascii "turtle.txt"
+else
+  render_ascii "jellyfish.txt"
+end
+yes_or_no(answer, "turtles", "Hold on tight! We are going for a swim!", "Oh no! There's too many tentacles!")
 answer= deep_sea answer
-yes_or_no(answer, "towards", "You see an angler fish and it starts to chase you", "you see a glowing squid and follow it")
+
+if answer == "towards"
+  render_ascii "fish.txt"
+else
+  render_ascii "squid.txt"
+end
+
+yes_or_no(answer, "towards", "You see an angler fish and it starts to chase you", "You see a glowing squid and follow it")
+
 answer = mantaray_dolphin
+if answer == 'left'
+  render_ascii "mantaray.txt"
+else
+  render_ascii "dolphin.txt"
+end
 yes_or_no(answer, "left", "You found a manta ray! Ride it to the surface to find shore.", "You found a dolphin! Hold on tight and it will take you to the surface.")
 safe
